adce1bd4e9d80bf2593a00483cc7bcf4b82c7673
mrjay <mrjay@janotech.com>
2024-07-11 17:05:13 -0400

    Fix test class

--- sb/test_class.py
+++ sb/test_class.py
@@ -7,8 +7,4 @@ class TestClass(Widget):
     def __init__(self, **kwargs):
         super(TestClass, self).__init__(**kwargs)
         self.name = ""
-        #self.keys = 88
-        #self.label = Label(text = 'TestClass', pos_hint = {'top': 1}, size_hint = (1, 1))
-        #self.add_widget(self.label)
-        pass
 
